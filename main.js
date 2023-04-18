574ee7b461eb6f9ff3e5869f024de18c05826736
Konradnowy <konradnowy@wp.pl>
2023-04-18 15:59:16 +0200

    Poprawienie kodu

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ const age = 32;
 console.log(firstName);
 console.log(age);
 console.log(`Siema, nazwywam się ${firstName} i mam ${age} lata.`);
+
+function calculate (myNumber) {
+    console.log(`Dostałem ${myNumber}`);
+    return myNumber*7;
+}
+
+
+const myResult = calculate(age);
+
+console.log(myResult);
